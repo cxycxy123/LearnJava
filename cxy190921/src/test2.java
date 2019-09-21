3c4afb719c9d368179b65b1218a35e5892b7bc9c
Xinyu <3150102205@zju.edu.cn>
2019-09-21 09:55:21 +0800

    try

---
+++ cxy190921/src/test2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class test2
+{
+	@SuppressWarnings("resource")
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		int x = in.nextInt();
+		
+		System.out.println("Hello, Java! "+x);
+	}
+}
